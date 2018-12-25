609c6d7bdcf48c97557cd4c9e8b1cd1a1e1e9c72
Alamelu1812 <43254091+Alamelu1812@users.noreply.github.com>
2018-12-25 12:23:25 -0500

    Update MaximumSubArray.java

--- Arrays/MaximumSubArray.java
+++ Arrays/MaximumSubArray.java
@@ -1,4 +1,9 @@
 
+/*
+*Given an integer array nums, find the contiguous subarray (containing at least one number) 
+*which has the largest sum and return its sum.
+*/
+
 public class MaximumSubArray {
     public int maxSubArray(int[] nums) {
         //Kadane Algorithm
